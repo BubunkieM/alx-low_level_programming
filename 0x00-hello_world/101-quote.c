f0f8d7b73b5914e7b7c374beb394a2d0458fe5ce
BubunkieM <daisynontlantla.m@gmail.com>
2023-09-07 10:13:51 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,1 +1,13 @@
-#
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - printing without printf and puts
+ *
+ * Return: always (1)
+ *
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
