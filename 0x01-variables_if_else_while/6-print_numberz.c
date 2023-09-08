ad40550c16a3eaab4ee28604099b68f9323bb124
BubunkieM <daisynontlantla.m@gmail.com>
2023-09-08 03:13:19 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main -single digit numberz on base10 starting from 0
+ *
+ * Return: always (0)
+ *
+ */
+int main(void)
+{
+	int c;
+
+	c = 0;
+	while
+		(c < 10) {
+			putchar(c + '0');
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
