cb888c5950576972a2c78a633923d706c3cf60a8
BubunkieM <daisynontlantla.m@gmail.com>
2023-09-08 03:04:10 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main -printing single digits number base10 starting from 0
+ *
+ * Return: always (0)
+ *
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+		printf("%d", a);
+	putchar('\n');
+	return (0);
+}
+
