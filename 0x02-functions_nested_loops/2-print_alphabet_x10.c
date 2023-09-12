264f591b3127b8cf09b4f9667cc032a94c121d4e
BubunkieM <daisynontlantla.m@gmail.com>
2023-09-12 10:15:01 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - prints 10 times the alphabet in lowercase
+ */
+void print_alphabet_x10(void)
+{
+	char d;
+	int b;
+
+	b = 0;
+
+	while(b < 10)
+	{
+		d = 'a'
+		while (d <= 'z')
+		{
+			putchar(d);
+			d++
+		}
+		_putchar('\n');
+		b++;
+	}
+}
