11b460178ad026e464f788966957aab0ff8e0fb9
BubunkieM <daisynontlantla.m@gmail.com>
2023-10-24 08:24:57 -0700

    7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -6,7 +6,7 @@
  * @index: node desired position to retrieve
  * Return: pointer to the retrieved node
  */
-listint_t *get_nodeint_at_index(listint_y *head, unsigned int index)
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int a;
 
