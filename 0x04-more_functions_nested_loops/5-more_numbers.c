0c399cebc7302b310485f79f8489f0d18962360a
BubunkieM <daisynontlantla.m@gmail.com>
2023-10-29 05:41:04 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * more_numbers - prints 10 times the number from 0 through 14
+ * more_numbers - prints 10 times the numbers, from 0 to 14
  * followed by a new line
  */
 
@@ -18,4 +18,5 @@ for (i = 0; i < 10; i++)
 		_putchar(j % 10 + '0');
 	}
 	_putchar('\n');
+}
 }
